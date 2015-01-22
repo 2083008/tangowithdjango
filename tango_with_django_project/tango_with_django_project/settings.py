f5329683445d4d8ab596b12960a6d6da10797211
Alex Smith - 2083008 <a.smithycchs@gmail.com>
2015-01-22 21:37:16 +0000

    basic template implemented

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import pdb
 
+#pdb.set_trace()
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -21,6 +24,20 @@ SECRET_KEY = 'i18-m1i5%izk!fsq^b(v*#oh0km40pv+jraa)&+=-y7f1xt96#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = [
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+]
+
+
+#TEMPLATE_DIRS = (
+#    '/Users/alexsmith/WebDev/tangowithdjango/tango_with_django_project/rango/templates',
+#)
 
 TEMPLATE_DEBUG = True
 
