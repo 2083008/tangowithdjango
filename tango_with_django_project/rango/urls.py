26e385bd10a65419e6d3a8d70dc97eb7176d5603
Alex Smith - 2083008 <a.smithycchs@gmail.com>
2015-02-28 23:00:09 +0000

    Chapter 11 users can now change their passwords

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 import views  # from rango import views
 
+
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
     #url(r'^register/$', views.register, name='register'),
     #url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/', views.restricted, name='restricted'),
+
     #url(r'^logout/$', views.user_logout, name='logout')
 
 )
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 # At the top of your urls.py file, add the following line:
 from django.conf import settings
 from registration.backends.simple.views import RegistrationView
+from django.contrib.auth.views import password_reset
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self,request, user):
@@ -20,6 +22,12 @@ urlpatterns = patterns('',
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
    # url(r'^accounts/', include('django.contrib.auth.urls')),
     (r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^password/change/$',
+        auth_views.password_change,
+        name='auth_password_change'),
+    url(r'^password/change/done/$',
+        auth_views.password_change_done,
+        name='password_change_done'),
 )
 
 if settings.DEBUG:
